38900e20882dc18804183c67ead7111909e8390f
Shaik <peershaik786@gmail.com>
2024-12-11 14:00:25 +0530

    Latest changes

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Resume = () => {
     const onButtonClick = () => {
-        const pdfUrl = "Sample.pdf";
+        const pdfUrl = "./Senior-UI-Designer-Developer-PeeraShaik.pdf";
         const link = document.createElement("a");
         link.href = pdfUrl;
-        link.download = "../Senior-UI-Designer-Developer-PeeraShaik.pdf"; // specify the filename
+        link.download = "Senior-UI-Designer-Developer-PeeraShaik.pdf";
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
